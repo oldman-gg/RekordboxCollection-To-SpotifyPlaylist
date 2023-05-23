5a23e6414f0cd43707b01b2c1d2c80669ca9a6c2
wyattnorris <wyattfnorris@gmail.com>
2023-05-22 23:40:10 -0400

    Added comments on where to find credentials.

--- Spotify_Main.py
+++ Spotify_Main.py
@@ -1,29 +1,29 @@
 import pandas as pd
 import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyOAuth
 from fuzzywuzzy import fuzz
 
-#Import Spotify API credentials and Rekordbox .csv
-from environment_variables import spotify_client_id, spotify_client_secret, rekordbox_csv_input, output
+#import Spotify API credentials and Rekordbox .csv
+from environment_variables import spotify_client_id, spotify_client_secret, rekordbox_csv_input, output, username, playlist_id, redirect_uri
 
-#Set up Spotify client
-client_credentials_manager = SpotifyClientCredentials(client_id=spotify_client_id, client_secret=spotify_client_secret)
-sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+#set up Spotify client
+scope = "playlist-modify-private"
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=spotify_client_id, client_secret=spotify_client_secret, redirect_uri=redirect_uri, scope=scope, username=username))
 
-# Read the CSV file
+#read Rekordbox CSV file
 try:
     df = pd.read_csv(rekordbox_csv_input)
 except FileNotFoundError:
     print(f"File {rekordbox_csv_input} not found.")
     exit()
 
-#Create a new column 'spotify_uri' and initialize it with None
+#create a new column 'spotify_uri' and initialize it with None
 df['spotify_uri'] = None
 
-#Create a list of queries
+#create a list of queries
 queries = [f"{row['Artist']} {row['Track Title']}" for index, row in df.iterrows()]
 
-# Iterate over the queries
+#iterate over the queries
 for index, query in enumerate(queries):
     try:
         # Search for the song on Spotify
@@ -32,15 +32,20 @@ for index, query in enumerate(queries):
         print(f"An error occurred: {e}")
         continue
 
-    #Check if the song is available and update the 'spotify_uri' column accordingly
+    #check if the song is available and update the 'spotify_uri' column accordingly
     for track in results['tracks']['items']:
-        #Use fuzzy matching to compare the song name in the CSV file with the song name on Spotify
+        #use fuzzy matching to compare the song name in the CSV file with the song name on Spotify
         spotify_song = track['name']
         spotify_artist = track['artists'][0]['name']  #assuming the first artist is the main artist
         spotify_query = f'{spotify_artist} {spotify_song}'
-        if fuzz.ratio(query.lower(), spotify_query.lower()) > 95:  #you can adjust the threshold as needed
+        if fuzz.ratio(query.lower(), spotify_query.lower()) > 90:  #you can adjust the threshold as needed
             df.loc[index, 'spotify_uri'] = track['uri']
+            # Add track to the playlist
+            try:
+                sp.user_playlist_add_tracks(username, playlist_id, [track['uri']])
+            except Exception as e:
+                print(f"Failed to add track to playlist: {e}")
             break  #stop checking other tracks once a match is found
 
 # Write the DataFrame back to the CSV file
-df.to_csv(output, index=False)
+df.to_csv(output, index=False)  #replace with your rekordbox file
--- environment_variables_git.py
+++ environment_variables_git.py
@@ -2,6 +2,10 @@
 rekordbox_csv_input = '.csv'
 output = '.csv'
 
-#Spotify - #found in Spotify dev dashboard https://developer.spotify.com/
-spotify_client_id = ''
-spotify_client_secret = ''
+#Spotify credentials
+spotify_client_id = '' #found in Spotify dev dashboard
+spotify_client_secret = ''#found in Spotify dev dashboard
+username = ''
+playlist_name = ''
+playlist_id = ''
+redirect_uri = 'http://localhost:8080/callback'
