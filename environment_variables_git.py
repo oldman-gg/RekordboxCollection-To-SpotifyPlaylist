bfd8794bbe845ae4b3b61a43979c42e286b93da2
wyattnorris <wyattfnorris@gmail.com>
2023-05-22 22:48:44 -0400

    Added comments on where to find credentials.

--- environment_variables_git.py
+++ environment_variables_git.py
@@ -2,6 +2,6 @@
 rekordbox_csv_input = '.csv'
 output = '.csv'
 
-#Spotify
-spotify_client_id = '' #found in Spotify dev dashboard
-spotify_client_secret = ''#found in Spotify dev dashboard
+#Spotify - #found in Spotify dev dashboard https://developer.spotify.com/
+spotify_client_id = ''
+spotify_client_secret = ''
